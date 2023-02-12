3e9de9e8f50bf19c62c8d8132ec15e0d44d7b59f
danielrobl3s <danielrobles200217@gmail.com>
2023-02-11 20:20:12 -0600

    version adulterada

--- automata.py
+++ automata.py
@@ -1,37 +1,6 @@
 state = 0
 transition = 0
-symbol = ''
 word = ''
-result = ''
+symbol = ''
 
-automata=[{
-    state: 1,
-    symbol: '0',
-    transition: 2
-},
-{
-    state: 2,
-    symbol: '0',
-    transition: 2
-},
-{
-    state: 2,
-    symbol: '1',
-    transition: 3
-}]
-
-def automataTransition(word):
-    result = ''
-    for i in word:
-        for j in range(len(automata)):
-            if i == str(automata[j][symbol]) and str(automata[j][transition]) != j:
-                result += i
-                print('letter' + word[j] + ' accepted, state: ' + str(j))
-            else:
-                print('word not accepted')
-                exit()
-    
-
-word = input('tell me a word')
-automataTransition(word)
-print('your word ' + result + ' is able to being made by the automata')
+automata = [{}, {}, {}, {}]
