911efe12bcd5185db3c6e31828cf7f505774ce68
danielrobl3s <danielrobles200217@gmail.com>
2023-02-11 22:50:04 -0600

    validacion primera de entrada lista

--- automata.py
+++ automata.py
@@ -1,25 +1,30 @@
 class State():
-    def __init__(self, number, stat =' ', transitions = []):
+    def __init__(self, number, stat =' ', transitions = {}):
         self.number = number
         self.transitions = transitions
         self.stat = stat
 
 def automataTransition(word):
     symbol = ''
-    for i in word:
-        for j in range(len(automata)):
-            print(str(automata[j].stat))
+    cstate = 0
+    for i in automata:
+        if '0' == word[0]:
+            pass
+        else:
+            print('not a valid word')
+            exit()
 
 word = ''
 symbol = ''
+cstate = 0
 
 automata = [
-    State(0, 'initial', [0]),
-    State(1, ' ', [0, 1]),
-    State(2, 'final', [])
+    State(0, 'initial', {0: 1}),
+    State(1, ' ', {0: 1, 1: 2}),
+    State(2, 'final', {})
 ]
 
 
 
-word = input('Put a word from this alphabet: 0 and 1: ')
+word = input('Put a word from this alphabet: 0 and 1:  ')
 automataTransition(word)
