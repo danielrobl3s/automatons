8bae873fa5cdcf6cf35a9157da7627e73829c4d1
danielrobl3s <danielrobles200217@gmail.com>
2023-02-11 23:45:41 -0600

    el mas avanzado hasta ahora

--- automata.py
+++ automata.py
@@ -1,30 +1,40 @@
+#Class to create an object 'state' which will be added to our automata list down below
 class State():
-    def __init__(self, number, stat =' ', transitions = {}):
+    def __init__(self, number, stat =' ', transitions = {}, symbols = {}):
         self.number = number
         self.transitions = transitions
+        self.symbols = symbols
         self.stat = stat
+#function to transition and store the symbols that make up the word
 
 def automataTransition(word):
     symbol = ''
-    cstate = 0
+    j = 0
     for i in automata:
-        if '0' == word[0]:
-            pass
-        else:
-            print('not a valid word')
-            exit()
+        keys = list(i.transitions.keys())
+        print(keys)
+
+        for w in word:
+            if word[w] in str(keys):
+                symbol += word[w]
+            elif i.stat == 'final':
+                print('done!')
+                exit()
+            else:
+                print('not a valid word')
+                exit()
+        j += 1
 
 word = ''
 symbol = ''
-cstate = 0
 
+#Automata variable is a list which stores a series of states objects
 automata = [
-    State(0, 'initial', {0: 1}),
-    State(1, ' ', {0: 1, 1: 2}),
-    State(2, 'final', {})
+    State(0, 'initial', {0: 1}, {1: 0}),
+    State(1, ' ', {0: 1, 1: 2}, {2:1}),
+    State(2, 'final', {}, {})
 ]
 
-
-
+#Entry point
 word = input('Put a word from this alphabet: 0 and 1:  ')
 automataTransition(word)
