6d398d900bf1d2c8905a3e50af946632e530a5e0
danielrobl3s <danielrobles200217@gmail.com>
2023-02-11 22:06:51 -0600

    modelo de clases listo

--- automata.py
+++ automata.py
@@ -8,28 +8,16 @@ def automataTransition(word):
     symbol = ''
     for i in word:
         for j in range(len(automata)):
-            if str(automata[j][transition]) == i:
-                symbol += i
-                print('symbol: ' + symbol)
-            else: 
-                exit()
+            print(str(automata[j].stat))
 
-istate = 0
-state = 0
-fstate = 0
-transition = 0
 word = ''
 symbol = ''
 
-st0 = any
-st1 = any
-st2 = any
-
-automata = {
-    st0: State(0, 'initial', [0]),
-    st1: State(1, ' ', [0,1]),
-    st2: State(2, 'final', [])
-}
+automata = [
+    State(0, 'initial', [0]),
+    State(1, ' ', [0, 1]),
+    State(2, 'final', [])
+]
 
 
 
